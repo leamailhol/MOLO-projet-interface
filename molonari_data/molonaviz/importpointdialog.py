969d61abc8713de93124537069dfb0cc8a1bf327
leamailhol <lea.mailhol@mines-paristech.fr>
2021-04-07 12:11:25 +0200

    point

---
+++ molonari_data/molonaviz/importpointdialog.py
@@ -0,0 +1,35 @@
+import sys
+import os
+from PyQt5 import QtWidgets, uic
+
+from point import Point
+
+From_ImportPointDialog,dummy = uic.loadUiType(os.path.join(os.path.dirname(__file__),"importpointdialog.ui"))
+
+class ImportPointDialog(QtWidgets.QDialog,From_ImportPointDialog):
+    def __init__(self):
+        # Call constructor of parent classes
+        super(ImportPointDialog, self).__init__()
+        QtWidgets.QDialog.__init__(self)
+        
+        self.setupUi(self)
+        
+        self.pushButton_BrowseRawTemperature.clicked.connect(self.browseRawTemp)
+        self.pushButton_BrowseRawPressure.clicked.connect(self.browseRawPres)
+        
+    def browseRawTemp(self):
+        dirPath = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Raw Temperature")
+        if dirPath:
+            self.lineEdit_RawTemperature.setText(dirPath) 
+            
+    def browseRawPres(self):
+        dirPath = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Raw Pressure")
+        if dirPath:
+            self.lineEdit_RawPressure.setText(dirPath)
+
+    def getPoint(self):
+        name = self.lineEditName.text()
+        rawTemp = self.lineEdit_RawTemperature.text()
+        rawPressure = self.lineEdit_RawPressure.text()
+        sensor = self.comboBox_Sensor.text()
+        return Point(name,sensor,rawTemp, rawPressure)
