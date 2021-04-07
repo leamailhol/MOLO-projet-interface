ee61f0db983dde3fd6dc85eff7b18e8f99ee221e
leamailhol <lea.mailhol@mines-paristech.fr>
2021-04-07 12:10:52 +0200

    point

---
+++ molonari_data/molonaviz/point.py
@@ -0,0 +1,10 @@
+import os
+from PyQt5 import QtGui, QtCore
+
+class Point(object):
+    
+    def __init__(self, name, sensor, rawTemp, rawPres):
+        self.name = name
+        self.sensor = sensor
+        self.rawTemp = rawTemp
+        self.rawPres = rawPres
