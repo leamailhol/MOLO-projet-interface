1edf8cd29a5180a32d5c7c9b1be26f00b7d0919e
Flore <flore.maroni@mines-paristech.fr>
2021-04-09 16:42:29 +0200

    treeview sensors

--- molonari_data/molonaviz/sensor.py
+++ molonari_data/molonaviz/sensor.py
@@ -1,6 +1,12 @@
 from numpy import NaN
 
-class Sensor(object):
+
+class sensorType(object):
+
+    def __init__(self, name=""):
+        self.name = name
+
+class pressureSensor(object):
     '''
     classdocs
     '''
@@ -10,3 +16,22 @@ class Sensor(object):
         self.intercept = intercept
         self.dudh = dudh
         self.dudt = dudt
+
+class temperatureSensor(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, name=""):
+        self.name = name
+
+
+class temperatureShaft(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, name="", t_sensor_name=NaN, sensors_depth=NaN):
+        self.name = name
+        self.t_sensor_name = t_sensor_name
+        self.sensors_depth = sensors_depth
