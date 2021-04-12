a61e0a88bd623578b30f3bf59b928c730c13199b
Maelle Baronnet <maelle.baronnet@mines-paristech.fr>
2021-04-12 14:40:36 +0200

    Fin de connection datapointview

--- molonari_data/molonaviz/datapointview.py
+++ molonari_data/molonaviz/datapointview.py
@@ -1,11 +1,11 @@
 import sys
 import os
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from point import Point
 import pandas as pd 
 from study import Study
 From_DataPointView,dummy = uic.loadUiType(os.path.join(os.path.dirname(__file__),"datapointview.ui"))
-
+from csv import reader
 #path_point = 'C:/Users/Léa/Documents/MINES 2A/MOLONARI/INTERFACE/MOLO-projet-interface/molonari_data/study_ordiLea/Point001'
 #os.chdir(path_point)
 
@@ -41,17 +41,66 @@ class DataPointView(QtWidgets.QDialog,From_DataPointView):
         os.chdir(path_point)
         self.setupUi(self)
 
+        # On paramètre le premier onglet
+
+        ## Notice
+
+        text_notice = ''
+
+        with open('imp_notice.csv', 'r', encoding='utf8') as read_obj:
+            # pass the file object to reader() to get the reader object
+            csv_reader = reader(read_obj)
+            # Iterate over each row in the csv using reader object
+            for row in csv_reader:
+                if len(row) > 0 and text_notice!='': 
+                    # row variable is a list that represents a row in csv
+                    text_notice = text_notice + '\n' + row[0]
+                if len(row) > 0 and text_notice == '':
+                    text_notice = row[0]
+
+
+        self.plainTextEditNotice.appendPlainText(text_notice)
+        self.plainTextEditNotice.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+
+
+        ## Geometrie 
+
+        print('')
+
+        text_geometry = ''
+
+        with open('imp_info.csv', 'r', encoding='utf8') as read_obj:
+            # pass the file object to reader() to get the reader object
+            csv_reader = reader(read_obj)
+            # Iterate over each row in the csv using reader object
+            for row in csv_reader:
+                if len(row) > 0 and text_geometry!='': 
+                    # row variable is a list that represents a row in csv
+                    word1, word2 = row[0].split(';') 
+                    text_geometry = text_geometry + '\n' + word1 + ' : ' + word2
+                if len(row) > 0 and text_geometry == '':
+                    word1, word2 = row[0].split(';') 
+                    text_geometry= 'Point_Name : ' + word2
+        
+        self.plainTextEditGeometry.appendPlainText(text_geometry)
+        self.plainTextEditGeometry.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+
+        ## Schéma 
+
+        self.InstallationImage.setPixmap(QtGui.QPixmap('imp_config.png'))
+
+
         self.pushButtonReset.clicked.connect(self.reset)
 
         self.pushButtonCleanup.clicked.connect(self.cleanup)
 
         self.pushButtonCompute.clicked.connect(self.compute)
 
-        #col_temp = ['Index','Date','Tension','Température','A','B','C']
+        
         col_temp = ['Date','Tension','Température']
         self.dataTemperature = pd.read_csv('imp_raw_pressure.csv', encoding='latin-1', sep=';', low_memory=False, skiprows=1)
         self.dataTemperature.columns = col_temp
-        #self.dataTemperature = self.dataTemperature.drop(['A','B','C'],axis=1)
+        
 
         print(self.dataTemperature)
         print(self.dataTemperature.columns)
