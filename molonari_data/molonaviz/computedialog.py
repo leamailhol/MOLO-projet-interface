bf289baf76b52165bac96ce65f7de16b23ab42cc
leamailhol <lea.mailhol@mines-paristech.fr>
2021-04-19 16:58:16 +0200

    quant

--- molonari_data/molonaviz/computedialog.py
+++ molonari_data/molonaviz/computedialog.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from PyQt5 import QtWidgets, uic
-
+import numpy as np
 From_ComputeDialog,dummy = uic.loadUiType(os.path.join(os.path.dirname(__file__),"computedialog.ui"))
 
 
@@ -11,4 +11,13 @@ class ComputeDialog(QtWidgets.QDialog,From_ComputeDialog):
         super(ComputeDialog, self).__init__()
         QtWidgets.QDialog.__init__(self)
         self.setupUi(self)
+        self.list_quantile = []
+
+        self.textEditQuantile.setText(f'Quantiles : {self.list_quantile}')
+        self.pushButton_Quantile.clicked.connect(self.addQuantile)
+
+    def addQuantile(self) :
+        self.list_quantile.append(np.around(float(self.doubleSpinBox_Quantile.value()),2))
+        self.textEditQuantile.setText(f'Quantiles : {self.list_quantile}')
+
      
