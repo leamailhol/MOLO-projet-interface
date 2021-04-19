d2f3321d7e2fd68de1825cc0dbda901165caa342
Flore <flore.maroni@mines-paristech.fr>
2021-04-19 14:36:09 +0200

    plot clean up

--- molonari_data/study_ordiFlore/Point001/test.py
+++ molonari_data/study_ordiFlore/Point001/test.py
@@ -2,9 +2,9 @@
 #dft = dft.iloc[15:]
 #dfp = dfp.iloc[15:]
 #Remove Spurious Temperatures (Kelvin)
-dft.iloc[:,4:]=np.where((dft.iloc[:,4:]<274), np.NaN, dft.iloc[:,4:])
-dft.iloc[:,4:]=np.where((dft.iloc[:,4:]>293), np.NaN, dft.iloc[:,4:])
+dft.iloc[:,3:]=np.where((dft.iloc[:,3:]<274), np.NaN, dft.iloc[:,3:])
+dft.iloc[:,3:]=np.where((dft.iloc[:,3:]>304), np.NaN, dft.iloc[:,3:])
 #Remove Spurious Head Differential (Meters)
-dfp.iloc[:,4:]=np.where((dfp.iloc[:,4:]>0.1), np.NaN, dfp.iloc[:,4:])
-dfp.iloc[:,4:]=np.where((dfp.iloc[:,4:]<-0.1), np.NaN, dfp.iloc[:,4:])
+dfp.iloc[:,3:]=np.where((dfp.iloc[:,3:]>0.1), np.NaN, dfp.iloc[:,3:])
+dfp.iloc[:,3:]=np.where((dfp.iloc[:,3:]<-0.1), np.NaN, dfp.iloc[:,3:])
 print("coucou")
