ff6d0372188fe096bb706cf1399e9e4fa47548a8
Flore <flore.maroni@mines-paristech.fr>
2021-04-19 11:13:42 +0200

    error dans clean up

--- molonari_data/molonaviz/dialogcleanup.py
+++ molonari_data/molonaviz/dialogcleanup.py
@@ -31,7 +31,7 @@ class DialogCleanUp(QtWidgets.QDialog,From_DialogCleanUp):
         exec(open("test.py").read())
 
         dft.to_csv('processed_temperature.csv', sep = ',')
-        dft.to_csv('processed_pressure.csv', sep = ',')
+        dfp.to_csv('processed_pressure.csv', sep = ',')
 
     def getCode(self):
         return self.textEdit.toPlainText()
